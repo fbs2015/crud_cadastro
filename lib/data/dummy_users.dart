5e93950aaeb814b3ff09f3e093d0f80e9141121f
fbs2015 <f.b.s2009@hotmail.com>
2022-02-21 10:29:53 -0300

    Criado model/user e mock dos dados

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:crud_cadastro/models/user.dart';
+
+final DUMMY_USERS = {
+  '1': User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@email.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2020/05/03/07/02/coronavirus-5124128_960_720.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@email.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2018/05/01/19/57/call-center-3366790_960_720.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@email.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2017/10/09/15/45/call-center-2833793_960_720.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'guilherme@email.com.br',
+    avatarUrl: '',
+  ),
+};
---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+
+
+class User{
+  String id;
+  final String name;
+  final String email;
+  final String avatarUrl;
+
+  User({
+    this.id ?? = '',
+    required this.name,
+    required this.email,
+    required this.avatarUrl
+  });
+}
