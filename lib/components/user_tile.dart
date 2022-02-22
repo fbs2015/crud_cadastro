a633291ad2b1baf006ebbe60f94fe850c982b6b6
fbs2015 <f.b.s2009@hotmail.com>
2022-02-22 15:50:57 -0300

    Alteração final em user_tile e user_form. Fim do exercicio

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,6 +1,8 @@
 import 'package:crud_cadastro/models/user.dart';
+import 'package:crud_cadastro/provider/user_provider.dart';
 import 'package:crud_cadastro/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {  
   final User user;
@@ -24,13 +26,37 @@ class UserTile extends StatelessWidget {
               onPressed: (){
                 Navigator.of(context).pushNamed(
                   AppRoutes.USER_FORM,
-                  arguments: user);
+                  arguments: user
+                );
               }, 
               color: Colors.orange,
               icon: Icon(Icons.edit)
             ),
             IconButton(
-              onPressed: (){}, 
+              onPressed: (){
+                showDialog(
+                  context: context, 
+                  builder: (context) => AlertDialog(
+                    title: Text('Excluir usuário'),
+                    content: Text('Tem certeza?'),
+                    actions: [
+                      FlatButton(
+                        onPressed: (){
+                          Navigator.of(context).pop();
+                        }, 
+                        child: Text('Não'),
+                      ),
+                       FlatButton(
+                        onPressed: (){
+                          Provider.of<UsersProvider>(context, listen: false).remove(user.id);
+                          Navigator.of(context).pop();
+                        }, 
+                        child: Text('Sim'),
+                       ),
+                    ],
+                  )
+                );                          
+              }, 
               color: Colors.red,
               icon: Icon(Icons.delete)
             ),
--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,28 +1,92 @@
-
-
+import 'package:crud_cadastro/models/user.dart';
+import 'package:crud_cadastro/provider/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class UserForm extends StatelessWidget {
-  const UserForm({ Key? key }) : super(key: key);
+class UserForm extends StatefulWidget { 
+
+  @override
+  State<UserForm> createState() => _UserFormState();
+}
+class _UserFormState extends State<UserForm> {
+  final _form = GlobalKey<FormState>();
+
+  final Map<String, Object> _formData = {};
+
+  void _loadFormData(User user){
+    _formData['id'] = user.id;
+    _formData['name'] = user.name;
+    _formData['email'] = user.email;
+    _formData['avatarUrl'] = user.avatarUrl;
+  }
+
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+
+     final user = ModalRoute.of(context)?.settings.arguments != null 
+     ? ModalRoute.of(context)?.settings.arguments  as User 
+     : User(name: '', email: '', avatarUrl: '');  
+    _loadFormData(user);   
+  }
 
   @override
   Widget build(BuildContext context) {
+   
+   
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de usuário'),
         actions: [
           IconButton(
             onPressed: (){
-              Navigator.of(context).pop();
+              final isValid = _form.currentState?.validate() ?? false;
+
+              if (isValid) {
+                _form.currentState?.save();
+                Provider.of<UsersProvider>(context, listen: false).put(User(
+                  id: _formData['id'].toString(),
+                  name: _formData['name'].toString(), 
+                  email: _formData['email'].toString(), 
+                  avatarUrl: _formData['avatarUrl'].toString()));
+                Navigator.of(context).pop();
+              }
             }, 
             icon: Icon(Icons.save)
           ),
         ],
       ),    
       body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Form(
-          child: Column(),
+        padding: EdgeInsets.all(20),
+        child: Form(          
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                initialValue: _formData['name'].toString(),
+                decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if(value == null || value.isEmpty){
+                    return 'Nome inválido';
+                  }
+                  if(value.trim().length < 3){
+                    return 'Nome muito pequeno. Mínimo 3 letras.';
+                  }
+                },
+                onSaved: (value) => _formData['name'] = value.toString(),
+              ),
+              TextFormField(
+                initialValue: _formData['email'].toString(),
+                decoration: InputDecoration(labelText: 'E-mail'),
+                onSaved: (value) => _formData['email']  =value.toString(),
+              ),
+              TextFormField(
+                initialValue: _formData['avatarUrl'].toString(),
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarUrl'] = value.toString(),
+              ),
+            ],
+          ),
         ),
       ),  
     );
